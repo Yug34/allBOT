418ffee1bc0dce94dab33d1092e855db7b4d61a4
Yug34 <vertilax1337@gmail.com>
2020-12-13 19:31:54 +0530

    Changed userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 const { getUserFromMention } = require("../util/getUser");
 
 module.exports = {
@@ -7,11 +8,26 @@ module.exports = {
     const split = message.content.split(/ +/);
     const args = split.slice(1);
 
-    const user = getUserFromMention(args[0], client);
-    message.channel.send(
-      `Name: ${user.username}, ID: ${user.id}, Avatar: ${user.displayAvatarURL({
-        dynamic: true,
-      })}`
-    );
+    if (args.length === 0) {
+      const avatarEmbed = new Discord.MessageEmbed()
+        .setColor("#00ff00")
+        .setTitle(message.author.username)
+        .setImage(message.author.displayAvatarURL())
+        .setTimestamp()
+        .addField("ID", message.author.id);
+
+      message.channel.send(avatarEmbed);
+    }
+    else {
+      const user = getUserFromMention(args[0], client);
+      const avatarEmbed = new Discord.MessageEmbed()
+          .setColor("#00ff00")
+          .setTitle(user.username)
+          .setImage(user.displayAvatarURL())
+          .setTimestamp()
+          .addField("ID", user.id);
+
+      message.channel.send(avatarEmbed);
+    }
   },
 };
