2d3db6b5aa4877bf06f62560ee70c29b7f28e76f
Yug34 <vertilax1337@gmail.com>
2020-12-13 19:10:15 +0530

    Added avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: "avatar",
+  description: "Send an embed of avatar!",
+  execute(message) {
+    if (message.content === "!avatar") {
+      const avatarEmbed = new Discord.MessageEmbed()
+        .setColor("#00ff00")
+        .setTitle(message.author.username)
+        .setImage(message.author.displayAvatarURL())
+        .setTimestamp();
+
+      message.channel.send(avatarEmbed);
+    } else if (message.mentions.users) {
+      message.mentions.users.forEach((user) => {
+        const avatarEmbed = new Discord.MessageEmbed()
+          .setColor("#00ff00")
+          .setTitle(user.username)
+          .setImage(user.displayAvatarURL())
+          .setTimestamp();
+
+        message.channel.send(avatarEmbed);
+      });
+    }
+  },
+};
