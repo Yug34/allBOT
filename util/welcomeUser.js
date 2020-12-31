5a52b82231a0606872aef5f239f4dbf0767b11ac
Yug34 <vertilax1337@gmail.com>
2020-12-31 22:06:03 +0530

    welcomeUser.js refactor

--- util/welcomeUser.js
+++ util/welcomeUser.js
@@ -1,49 +1,64 @@
-const Canvas = require('canvas');
+const Canvas = require("canvas");
 const Discord = require("discord.js");
 
 exports.welcome = async (member) => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
-    if (!channel) return;
+  const channel = member.guild.channels.cache.find(
+    (ch) => ch.name === "member-log"
+  );
+  if (!channel) return;
 
-    const canvas = Canvas.createCanvas(700, 250);
-    const ctx = canvas.getContext('2d');
+  const canvas = Canvas.createCanvas(700, 250);
+  const ctx = canvas.getContext("2d");
 
-    const background = await Canvas.loadImage('./images/wallpaper.jpg');
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  const background = await Canvas.loadImage("./images/wallpaper.jpg");
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-    ctx.strokeStyle = '#74037b';
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = "#ca42de";
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-    ctx.font = '28px sans-serif';
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText(`Welcome to ${member.guild.name},`, canvas.width / 2.5, canvas.height / 3.5);
+  ctx.font = "28px sans-serif";
+  ctx.fillStyle = "#ffffff";
+  ctx.fillText(
+    `Welcome to ${member.guild.name},`,
+    canvas.width / 2.5,
+    canvas.height / 3.5
+  );
 
-    ctx.font = fontSize(canvas, `${member.displayName}!`);
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+  ctx.font = fontSize(canvas, `${member.displayName}!`);
+  ctx.fillStyle = "#ffffff";
+  ctx.fillText(
+    `${member.displayName}!`,
+    canvas.width / 2.5,
+    canvas.height / 1.8
+  );
 
-    ctx.beginPath();
-    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.clip();
+  ctx.beginPath();
+  ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.clip();
 
-    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-    ctx.drawImage(avatar, 25, 25, 200, 200);
+  const avatar = await Canvas.loadImage(
+    member.user.displayAvatarURL({ format: "jpg" })
+  );
+  ctx.drawImage(avatar, 25, 25, 200, 200);
 
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+  const attachment = new Discord.MessageAttachment(
+    canvas.toBuffer(),
+    "welcome-image.png"
+  );
 
-    channel.send(`Welcome to the server, ${member}!`, attachment);
-}
+  channel.send(`Welcome to the server, ${member}!`, attachment);
+};
 
 const fontSize = (canvas, text) => {
-    const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext("2d");
 
-    let fontSize = 70;
-    do {
-        // Assign the font to the context and decrement it so it can be measured again
-        ctx.font = `${fontSize -= 10}px sans-serif`;
-        // Compare pixel width of the text to the canvas minus the approximate avatar size
-    } while (ctx.measureText(text).width > canvas.width - 300);
+  let fontSize = 70;
+  do {
+    // Assign the font to the context and decrement it so it can be measured again
+    ctx.font = `${(fontSize -= 10)}px sans-serif`;
+    // Compare pixel width of the text to the canvas minus the approximate avatar size
+  } while (ctx.measureText(text).width > canvas.width - 300);
 
-    return ctx.font;
-}
+  return ctx.font;
+};
