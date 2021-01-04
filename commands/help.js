041f456057fc8fc78e60df80f3d495ac62f057b3
Yug34 <vertilax1337@gmail.com>
2021-01-04 18:34:30 +0530

    help.js fix

--- commands/help.js
+++ commands/help.js
@@ -5,15 +5,34 @@ module.exports = {
   description: "List all available commands.",
   execute(message) {
     let str = "```css\n";
-    const commandFiles = fs
-      .readdirSync("./commands")
-      .filter((file) => file.endsWith(".js"));
 
+    const getDirectories = fs
+      .readdirSync("./commands", { withFileTypes: true })
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name);
+
+    for (const directory of getDirectories) {
+      str += `${directory}:\n`;
+      const commandFiles = fs
+        .readdirSync(`./commands/${directory}`)
+        .filter((file) => file.endsWith(".js"));
+      for (const file of commandFiles) {
+        const command = require(`./${directory}/${file}`);
+        str += command.name + " - " + command.description + "\n";
+      }
+    }
+
+    str += "Other commands:\n";
+    const commandFiles = fs
+        .readdirSync("./commands")
+        .filter((file) => file.endsWith(".js"));
+
+    str += "";
     for (const file of commandFiles) {
       const command = require(`./${file}`);
-      // str += `Name: ${command.name}, Description: ${command.description} \n`;
-      str += command.name + " - " + command.description + "\n"
+      str += command.name + " - " + command.description + "\n";
     }
+
     str += "```";
 
     message.channel.send(str);
